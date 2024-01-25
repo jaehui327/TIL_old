7c46e09482d409085a1b89e82d02276a57b14925
jaehui327 <jaeheekim970327@gmail.com>
2024-01-25 16:43:15 +0900

    [Algorithm] BOJ 2743 단어길이재기

---
+++ Algorithm/src/String/Main_B_2743_단어길이재기.java
@@ -0,0 +1,10 @@
+package String;
+
+import java.io.*;
+public class Main_B_2743_단어길이재기 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().toCharArray().length);
+        br.close();
+    }
+}
